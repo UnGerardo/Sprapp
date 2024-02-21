83c12e197610681e624aadbb6344406e483bf88f
UnGerardo <gerardojdls@gmail.com>
2024-02-20 17:19:02 -0800

    Create Deck and Card class models

---
+++ models/Card.js
@@ -0,0 +1,8 @@
+
+export default class Card {
+  constructor({ front, back, date }) {
+    this.front = front;
+    this.back = back;
+    this.date = date;
+  }
+}
---
+++ models/Deck.js
@@ -0,0 +1,8 @@
+
+export default class Deck {
+  constructor({ name }) {
+    this.name = name;
+    this.totalCards = 0;
+    this.cards = [];
+  }
+}
