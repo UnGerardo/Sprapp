a6caff090518b0eceb5b716b9c3c47fe96c9210b
UnGerardo <gerardojdls@gmail.com>
2024-02-22 14:58:18 -0800

    Update Card and Deck class models

--- models/Card.js
+++ models/Card.js
@@ -1,11 +1,12 @@
 
 export default class Card {
-  constructor({ front, back, date }) {
+  constructor({ id = crypto.randomUUID(), front = '', back = '', date = '', interval = 0, repetitions = 0, easeFactor = 2.5}) {
+    this.id = id;
     this.front = front;
     this.back = back;
     this.date = date;
-    this.interval = 0;
-    this.repetitions = 0;
-    this.easeFactor = 2.5;
+    this.interval = interval;
+    this.repetitions = repetitions;
+    this.easeFactor = easeFactor;
   }
 }
--- models/Deck.js
+++ models/Deck.js
@@ -1,8 +1,23 @@
 
 export default class Deck {
-  constructor({ name }) {
+  constructor({ name = '', cards = [] }) {
     this.name = name;
-    this.totalCards = 0;
-    this.cards = [];
+    this.cards = cards;
+  }
+
+  changeName(name) {
+    this.name = name;
+  }
+
+  addCard(card) {
+    this.cards.push(card);
+
+    return card.id === this.cards[this.cards.length - 1].id;
+  }
+
+  removeCard(cardId) {
+    console.log(this.cards);
+    this.cards = this.cards.filter(card => card.id !== cardId);
+    console.log(this.cards);
   }
 }
